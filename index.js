595bcb184346e6c70f1d0f790f439d16fd55e11e
jassyjr <116488124+jassyjr@users.noreply.github.com>
2023-01-28 09:16:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ for ( var i=0; i<numberOfButtons; i++) {
             var buttonInnerHTML = this.innerHTML;
             switch (buttonInnerHTML) {
                 case "l":
-                   var tom1= new Audio("sounds/tom-1.mp3");
+                   var tom1= new Audio("/sounds/tom-1.mp3");
                     tom1.play();
                     break;
                     
                         case "k":
-                   var tom2= new Audio("sounds/tom-2.mp3");
+                   var tom2= new Audio("/sounds/tom-2.mp3");
                     tom2.play();
                     break;
                     case "s":
-                   var tom3= new Audio("sounds/tom-3.mp3");
+                   var tom3= new Audio("/sounds/tom-3.mp3");
                     tom3.play();
                     break;
                     case "j":
@@ -21,15 +21,15 @@ for ( var i=0; i<numberOfButtons; i++) {
                     tom4.play();
                     break;
                     case "d":
-                  var snare = new Audio("sounds/snare.mp3");
+                  var snare = new Audio("/sounds/snare.mp3");
                     snare.play();
                     break;
                     case "a":
-                   var kickbass= new Audio("sounds/kick-bass.mp3");
+                   var kickbass= new Audio("/sounds/kick-bass.mp3");
                     kickbass.play();
                     break;
                     case "w":
-                   var crash= new Audio("sounds/crash.mp3");
+                   var crash= new Audio("/sounds/crash.mp3");
                     crash.play();
                     break;
                 default
